8e0ecb670b5f88767e2a592abdc4d474e82c27f7
Matthias Kestenholz <mk@feinheit.ch>
2024-03-11 20:27:05 +0100

    Add some QoL improvements to the model field

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -5,6 +5,9 @@ Next version
 ~~~~~~~~~~~~
 
 - Fixed the initialization in Django admin inlines.
+- Added a server-side sanitization callback to the ``ProseEditorField``.
+- Automatically added a ``get_*_excerpt`` model method to models using the
+  ``ProseEditorField`` as a convenience.
 
 
 `0.1`_ (2024-03-11)
--- django_prose_editor/fields.py
+++ django_prose_editor/fields.py
@@ -1,17 +1,42 @@
 from django import forms
 from django.db import models
+from django.utils.html import strip_tags
+from django.utils.text import Truncator
 
 from django_prose_editor.widgets import ProseEditorWidget
 
 
+def _identity(x):
+    return x
+
+
 class ProseEditorField(models.TextField):
-    def formfield(self, **kwargs):
-        defaults = {"form_class": ProseEditorFormField} | kwargs
-        return super().formfield(**defaults)
+    def __init__(self, *args, **kwargs):
+        self.sanitize = kwargs.pop("sanitize", _identity)
+        super().__init__(*args, **kwargs)
+
+    def clean(self, value, instance):
+        return self.sanitize(super().clean(value, instance))
+
+    def contribute_to_class(self, cls, name, **kwargs):
+        """Add a ``get_*_excerpt`` method to models which returns a
+        de-HTML-ified excerpt of the contents of this field"""
+        super().contribute_to_class(cls, name, **kwargs)
+        setattr(
+            cls,
+            f"get_{name}_excerpt",
+            lambda self, words=10, truncate=" ...": Truncator(
+                strip_tags(getattr(self, name))
+            ).words(words, truncate=truncate),
+        )
 
     def deconstruct(self):
         name, _path, args, kwargs = super().deconstruct()
         return (name, "django.db.models.TextField", args, kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {"form_class": ProseEditorFormField} | kwargs
+        return super().formfield(**defaults)
 
 
 class ProseEditorFormField(forms.CharField):
