bda4bc041bd9094b527ed776bf263411fdf66131
Matthias Kestenholz <mk@feinheit.ch>
2024-11-25 14:40:05 +0100

    Notify when rebuilding is successful

--- esbuild.mjs
+++ esbuild.mjs
@@ -9,7 +9,21 @@ const ctx = await esbuild.context({
   target: "es6",
   format: "iife",
   globalName: "DjangoProseEditor",
-  plugins: [postcss()],
+  plugins: [
+    postcss(),
+    {
+      name: "rebuild-notify",
+      setup(build) {
+        build.onEnd((result) => {
+          if (result.errors.length) {
+            console.error(result.errors)
+          } else {
+            console.debug("Rebuild succesful.")
+          }
+        })
+      },
+    },
+  ],
   outdir: "django_prose_editor/static/django_prose_editor/",
   // I really like sourcemaps, but I don't want to distribute them here.
   // People can check the open source package after all.
