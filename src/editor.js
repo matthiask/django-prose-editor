baa9e96d9a19e484f514c4f24513129f11a9a5b7
Matthias Kestenholz <mk@feinheit.ch>
2024-11-26 15:15:44 +0100

    Reorder and group imports and extensions

--- src/editor.js
+++ src/editor.js
@@ -1,26 +1,26 @@
 import "./editor.css"
 
 import { Editor } from "@tiptap/core"
-import { Subscript } from "@tiptap/extension-subscript"
-import { Superscript } from "@tiptap/extension-superscript"
-import { Link as BaseLink } from "@tiptap/extension-link"
+import { Document } from "@tiptap/extension-document"
+import { Dropcursor } from "@tiptap/extension-dropcursor"
+import { Gapcursor } from "@tiptap/extension-gapcursor"
+import { History } from "@tiptap/extension-history"
+import { Paragraph } from "@tiptap/extension-paragraph"
+import { Text } from "@tiptap/extension-text"
 
 import { Blockquote } from "@tiptap/extension-blockquote"
 import { Bold } from "@tiptap/extension-bold"
 import { BulletList } from "@tiptap/extension-bullet-list"
-import { Document } from "@tiptap/extension-document"
-import { Dropcursor } from "@tiptap/extension-dropcursor"
-import { Gapcursor } from "@tiptap/extension-gapcursor"
 import { HardBreak } from "@tiptap/extension-hard-break"
 import { Heading } from "@tiptap/extension-heading"
-import { History } from "@tiptap/extension-history"
 import { HorizontalRule } from "@tiptap/extension-horizontal-rule"
 import { Italic } from "@tiptap/extension-italic"
+import { Link as BaseLink } from "@tiptap/extension-link"
 import { ListItem } from "@tiptap/extension-list-item"
 import { OrderedList } from "@tiptap/extension-ordered-list"
-import { Paragraph } from "@tiptap/extension-paragraph"
 import { Strike } from "@tiptap/extension-strike"
-import { Text } from "@tiptap/extension-text"
+import { Subscript } from "@tiptap/extension-subscript"
+import { Superscript } from "@tiptap/extension-superscript"
 import { Underline } from "@tiptap/extension-underline"
 
 import { addLink } from "./commands.js"
@@ -54,31 +54,34 @@ export function createEditor(textarea, config) {
     element: editor,
     editable: !textarea.hasAttribute("disabled"),
     extensions: [
-      isTypeEnabled("blockquote") && Blockquote,
-      isTypeEnabled("strong") && Bold,
-      isTypeEnabled("bullet_list") && BulletList,
       Document,
       Dropcursor,
       Gapcursor,
+      config.history && History,
+      Paragraph,
+      Text,
+      /* Nodes and marks */
+      isTypeEnabled("blockquote") && Blockquote,
+      isTypeEnabled("strong") && Bold,
+      isTypeEnabled("bullet_list") && BulletList,
       isTypeEnabled("hard_break") && HardBreak,
       isTypeEnabled("heading") && Heading,
-      config.history && History,
       isTypeEnabled("horizontal_rule") && HorizontalRule,
       isTypeEnabled("em") && Italic,
-      ListItem,
-      isTypeEnabled("ordered_list") && OrderedList,
-      Paragraph,
-      isTypeEnabled("strikethrough") && Strike,
-      Text,
-      Menu.configure({ config }),
-      NoSpellCheck,
-      isTypeEnabled("sub") && Subscript,
-      isTypeEnabled("sup") && Superscript,
-      isTypeEnabled("underline") && Underline,
       isTypeEnabled("link") &&
         Link.configure({
           openOnClick: false,
         }),
+      (isTypeEnabled("bullet_list") || isTypeEnabled("ordered_list")) &&
+        ListItem,
+      isTypeEnabled("ordered_list") && OrderedList,
+      isTypeEnabled("strikethrough") && Strike,
+      isTypeEnabled("sub") && Subscript,
+      isTypeEnabled("sup") && Superscript,
+      isTypeEnabled("underline") && Underline,
+      /* Other extensions */
+      Menu.configure({ config }),
+      NoSpellCheck,
       config.typographic ? Typographic : null,
     ].filter(Boolean),
     content: textarea.value,
