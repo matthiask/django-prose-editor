fd7fcefacb5f3952ae850e9134a7d7d13cbd00c1
Matthias Kestenholz <mk@feinheit.ch>
2024-11-28 15:45:24 +0100

    Format init.js and friends

--- django_prose_editor/static/django_prose_editor/init.js
+++ django_prose_editor/static/django_prose_editor/init.js
@@ -1,41 +1,39 @@
 const config = document.currentScript.dataset
 
 import(config.editorJs).then((DjangoProseEditor) => {
+  DjangoProseEditor.messages = JSON.parse(config.messages)
 
-DjangoProseEditor.messages = JSON.parse(config.messages)
+  const marker = "data-django-prose-editor"
 
-const marker = "data-django-prose-editor"
-
-function initializeDjangoProseEditor(container) {
-  for (let el of container.querySelectorAll(`[${marker}]`)) {
-    if (!el.id.includes("__prefix__")) {
-      DjangoProseEditor.createEditor(el, JSON.parse(el.getAttribute(marker)))
-      el.removeAttribute(marker)
+  function initializeDjangoProseEditor(container) {
+    for (const el of container.querySelectorAll(`[${marker}]`)) {
+      if (!el.id.includes("__prefix__")) {
+        DjangoProseEditor.createEditor(el, JSON.parse(el.getAttribute(marker)))
+        el.removeAttribute(marker)
+      }
     }
   }
-}
 
-function initializeDjangoInlines() {
-  let o
-  if ((o = window.django) && (o = o.jQuery)) {
-    o(document).on("formset:added", (e) => {
-      initializeDjangoProseEditor(e.target)
-    })
+  function initializeDjangoInlines() {
+    let o
+    if ((o = window.django) && (o = o.jQuery)) {
+      o(document).on("formset:added", (e) => {
+        initializeDjangoProseEditor(e.target)
+      })
+    }
   }
-}
 
-function onReady(callback) {
-  const d = document
-  if (d.readyState !== "loading") {
-    setTimeout(callback, 0)
-  } else {
-    document.addEventListener("DOMContentLoaded", callback, { once: true })
+  function onReady(callback) {
+    const d = document
+    if (d.readyState !== "loading") {
+      setTimeout(callback, 0)
+    } else {
+      document.addEventListener("DOMContentLoaded", callback, { once: true })
+    }
   }
-}
 
-onReady(() => {
-  initializeDjangoProseEditor(document)
-  initializeDjangoInlines()
-})
-
+  onReady(() => {
+    initializeDjangoProseEditor(document)
+    initializeDjangoInlines()
+  })
 })
