cd0db0715b725f54ba98ee475143c42a9033a80f
Matthias Kestenholz <mk@feinheit.ch>
2024-11-29 18:12:01 +0100

    Remove the debugging leftovers

--- src/link.js
+++ src/link.js
@@ -5,13 +5,12 @@ import { addLink } from "./commands.js"
 export const Link = BaseLink.extend({
   addKeyboardShortcuts() {
     return {
-      "Mod-k": ({ editor, ...rest }) => {
+      "Mod-k": ({ editor }) => {
         let e
         if ((e = window.event)) {
           /* Disable browser behavior of focussing the search bar or whatever */
           e.preventDefault()
         }
-        console.debug("editor", editor, rest)
         addLink(editor.view.state, editor.view.dispatch)
       },
     }
