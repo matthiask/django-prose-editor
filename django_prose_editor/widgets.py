78e04b8c3a8f0e5f024d0f432d895a5d166edad6
Matthias Kestenholz <mk@feinheit.ch>
2025-02-14 10:11:57 +0100

    Commit the importmap generating hack

--- django_prose_editor/widgets.py
+++ django_prose_editor/widgets.py
@@ -1,10 +1,119 @@
 import json
+from dataclasses import dataclass
 
 from django import forms
 from django.conf import settings
 from django.templatetags.static import static
+from django.utils.html import html_safe, json_script, mark_safe
 from django.utils.translation import gettext_lazy
-from js_asset import JS, JSON
+from js_asset import CSS, JS, JSON
+
+
+# See https://code.djangoproject.com/ticket/36104
+orig_forms_media_add = forms.Media.__add__
+
+
+def forms_media_add(self, other):
+    if type(other) is not forms.Media:
+        return NotImplemented
+    return orig_forms_media_add(self, other)
+
+
+forms.Media.__add__ = forms_media_add
+
+
+@html_safe
+@dataclass(eq=True)
+class ImportMapImport:
+    key: str
+    value: str
+    scope: str | None = None
+
+    def __hash__(self):
+        return hash((self.key, self.value, self.scope))
+
+    def __str__(self):
+        return ""
+
+
+class ExtendedMedia(forms.Media):
+    def __init__(self, assets=None):
+        self._asset_lists = []
+        if assets:
+            self._asset_lists.append(assets)
+
+    def render(self):
+        assets = self.merge(*self._asset_lists)
+
+        import_map_entries = [
+            asset for asset in assets if isinstance(asset, ImportMapImport)
+        ]
+
+        return mark_safe(
+            self.render_import_map(import_map_entries)
+            + "\n".join(asset.__html__() for asset in assets)
+        )
+
+    def render_import_map(self, entries):
+        if not entries:
+            return ""
+        map = {"imports": {}}
+        for entry in entries:
+            if entry.scope:
+                scope = map.setdefault("scopes", {}).setdefault(entry.scope, {})
+                scope[entry.key] = entry.value
+            else:
+                map["imports"][entry.key] = entry.value
+        return (
+            json_script(map).replace('type="application/json"', 'type="importmap"')
+            + "\n"
+        )
+
+    def _add_media(self, other, *, reverse):
+        combined = self.__class__()
+
+        if type(other) is forms.Media:
+            combined._asset_lists = []
+            if not reverse:
+                combined._asset_lists.extend(self._asset_lists)
+
+            for medium_css_list in other._css_lists:
+                for _medium, css_list in sorted(medium_css_list.items()):
+                    combined._asset_lists.append(
+                        [
+                            # FIXME include media
+                            CSS(css) if isinstance(css, str) else css
+                            for css in css_list
+                        ]
+                    )
+
+            for js_list in other._js_lists:
+                combined._asset_lists.append(
+                    [JS(js) if isinstance(js, str) else js for js in js_list]
+                )
+
+            if reverse:
+                combined._asset_lists.extend(self._asset_lists)
+
+            return combined
+
+        if type(other) is ExtendedMedia:
+            combined = self.__class__()
+
+            if reverse:
+                combined._asset_lists = [*other._asset_lists, *self._asset_lists]
+            else:
+                combined._asset_lists = [*self._asset_lists, *other._asset_lists]
+
+            return combined
+
+        return NotImplemented
+
+    def __add__(self, other):
+        return self._add_media(other, reverse=False)
+
+    def __radd__(self, other):
+        return self._add_media(other, reverse=True)
 
 
 class ProseEditorWidget(forms.Textarea):
@@ -15,7 +124,11 @@ class ProseEditorWidget(forms.Textarea):
 
     @property
     def base_media(self):
-        return forms.Media(
+        return ExtendedMedia(
+            [
+                ImportMapImport(key="square", value="./modules/shapes/square.js"),
+            ]
+        ) + forms.Media(
             css={
                 "all": [
                     "django_prose_editor/material-icons.css",
