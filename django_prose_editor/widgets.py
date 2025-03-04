c0bf6a2048019dbd6b01292a35050b6499eab85b
Matthias Kestenholz <mk@feinheit.ch>
2025-03-04 15:38:39 +0100

    We really need the importmap

--- django_prose_editor/widgets.py
+++ django_prose_editor/widgets.py
@@ -28,6 +28,7 @@ class ProseEditorWidget(forms.Textarea):
                 ]
             },
             js=[
+                importmap,
                 JSON(
                     {
                         "stylesheets": [
