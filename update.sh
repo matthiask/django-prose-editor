36cfc8693912a0b5979de1a58df726000f0dfd47
Matthias Kestenholz <mk@feinheit.ch>
2025-04-29 18:08:16 +0200

    Depend on the required extensions directly, without going through the starter kit

--- update.sh
+++ update.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 yarn upgrade \
 @tiptap/core@next \
-@tiptap/extension-code@next \
+@tiptap/extension-blockquote@next \
+@tiptap/extension-bold@next \
 @tiptap/extension-code-block@next \
+@tiptap/extension-code@next \
 @tiptap/extension-color@next \
+@tiptap/extension-document@next \
+@tiptap/extension-hard-break@next \
+@tiptap/extension-heading@next \
 @tiptap/extension-highlight@next \
+@tiptap/extension-horizontal-rule@next \
 @tiptap/extension-image@next \
+@tiptap/extension-italic@next \
 @tiptap/extension-link@next \
 @tiptap/extension-list@next \
+@tiptap/extension-paragraph@next \
+@tiptap/extension-strike@next \
 @tiptap/extension-subscript@next \
 @tiptap/extension-superscript@next \
 @tiptap/extension-table@next \
 @tiptap/extension-text-align@next \
 @tiptap/extension-text-style@next \
+@tiptap/extension-text@next \
 @tiptap/extension-underline@next \
 @tiptap/extensions@next \
-@tiptap/pm@next \
-@tiptap/starter-kit@next
+@tiptap/pm@next
